1cd024e0260e04cbfd36f291373486c57da6a3a9
student456773 <sbickel201@gmail.com>
2017-01-26 16:19:49 -0600

    added code to link the activities

--- app/src/main/java/csci4020/shawnbickel/assignment1/blackjack/BlackJackDetails.java
+++ app/src/main/java/csci4020/shawnbickel/assignment1/blackjack/BlackJackDetails.java
@@ -1,7 +1,11 @@
 package csci4020.shawnbickel.assignment1.blackjack;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class BlackJackDetails extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class BlackJackDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_black_jack_details);
+
+        Button extraInfo = (Button) findViewById(R.id.ExtraInformation);
+
+        assert extraInfo != null;
+        extraInfo.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Uri uri = Uri.parse("http://www.bicyclecards.com/how-to-play/blackjack/");
+                Intent c = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(c);
+            }
+        });
     }
 }
--- app/src/main/java/csci4020/shawnbickel/assignment1/blackjack/MainActivity.java
+++ app/src/main/java/csci4020/shawnbickel/assignment1/blackjack/MainActivity.java
@@ -1,7 +1,10 @@
 package csci4020.shawnbickel.assignment1.blackjack;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button PlayGame = (Button) findViewById(R.id.PlayGame);
+        Button MoreInformation = (Button) findViewById(R.id.LearnMore);
+
+        assert PlayGame != null;
+        PlayGame.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent a  = new Intent (MainActivity.this, BlackjackGame.class);
+                startActivity(a);
+            }
+        });
+
+        assert MoreInformation != null;
+        MoreInformation.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent b  = new Intent (MainActivity.this, BlackJackDetails.class);
+                startActivity(b);
+            }
+        });
     }
 }
